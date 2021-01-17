acdac8f5b03fe22af411c20c150a3bbb32cac032
Kaustubh Kulkarni <57136442+kaustubhk24@users.noreply.github.com>
2021-01-17 14:23:10 +0530

    Update ThumbnailGenerator.py

--- assets/ThumbnailGenerator.py
+++ assets/ThumbnailGenerator.py
@@ -1,3 +1,8 @@
+
+# We are currently taking screenshot of 00:00:10:000 's Frame you can change value if you need
+# If you want use this generator your videos must be named from 0 to the value of {number_of_videos} else it would not work..
+
+
 import subprocess
 
 number_of_videos=200
@@ -11,5 +16,3 @@ for id in range(number_of_videos):
     subprocess.call(['ffmpeg', '-i', video_input_path, '-ss', '00:00:10.000', '-vframes', '1', img_output_path])
 
 
-
-# We are currently taking screenshot of 00:00:10:000 's Frame you can change value if you need
